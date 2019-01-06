7ea164cd5a1af27c0d3088a4f8c56881234e4643
zhouyiqing <2168123379@qq.com>
2019-01-06 10:06:50 +0800

    web2

---
+++ weber/js/index.js
@@ -0,0 +1,280 @@
+			window.onload = function(){
+				var one_0 = document.getElementsByClassName('one_0')[0];
+				window.onscroll=function(){
+					var st = document.documentElement.scrollTop || document.body.scrollTop;
+					if(st>230){
+						one_0.style.position = 'fixed';			
+						one_0.style.backgroundColor="white";
+					}else{
+						one_0.style.position = 'static';
+					}
+				}
+			}
+			var rb2 = document.getElementsByClassName('rb2')[0];
+			rb2.style.left='0px';
+			var a1;
+			rb2.onmouseover=function(){
+				clearInterval(a1);
+				a1=setInterval(you1,1);
+			}
+			rb2.onmouseout=function(){
+				clearInterval(a1);
+				a1=setInterval(you11,1);
+			}
+			function you1(){  
+				var you1 = parseInt(rb2.style.left);
+				if(rb2.style.left=='-80px'){
+					clearInterval(a1);
+				}else{
+					rb2.style.left=you1-1+'px';
+				}
+			}
+			function you11(){
+				var you11 = parseInt(rb2.style.left);
+				if(rb2.style.left=='0px'){
+					clearInterval(a1);
+				}else{
+					rb2.style.left=you11+1+'px';
+				}
+			}
+			//second
+			var you112 = document.getElementsByClassName('rb2')[1];
+			you112.style.left='0px';
+			var a2;
+			function you2(){  
+				var you1 = parseInt(you112.style.left);
+				if(you112.style.left=='-80px'){
+					clearInterval(a2);
+				}else{
+					you112.style.left=you1-1+'px';
+				}
+			}
+			function you21(){
+				var you11 = parseInt(you112.style.left);
+				if(you112.style.left=='0px'){
+					clearInterval(a2);
+				}else{
+					you112.style.left=you11+1+'px';
+				}
+			}
+			you112.onmouseover=function(){
+				clearInterval(a2);
+				a2=setInterval(you2,1);
+			}
+			you112.onmouseout=function(){
+				clearInterval(a2);
+				a2=setInterval(you21,1);
+			}
+			//third
+			var you113 = document.getElementsByClassName('rb2')[2];
+			var erwei = document.getElementById('erwei');
+			var erwei1 = document.getElementById('erwei1');
+			erwei.appendChild(erwei1);
+			you113.style.left='0px';
+			var a3;
+			function you3(){  
+				var you1 = parseInt(you113.style.left);
+				if(you113.style.left=='-80px'){
+					clearInterval(a3);
+				}else{
+					you113.style.left=you1-1+'px';
+				}
+			}
+			function you31(){
+				var you11 = parseInt(you113.style.left);
+				if(you113.style.left=='0px'){
+					clearInterval(a3);
+				}else{
+					you113.style.left=you11+1+'px';
+				}
+			}
+			you113.onmouseover=function(){
+				erwei1.src = '../img/erwei.png';
+				erwei.setAttribute('class','erwei2')			
+				clearInterval(a3);
+				a3=setInterval(you3,1);
+			}
+			you113.onmouseout=function(){
+				erwei1.src = '../img/serwei.png';
+				erwei.setAttribute('class','erwei1');					
+				clearInterval(a3);
+				a3=setInterval(you31,1);
+			}
+			var you114 = document.getElementsByClassName('rb2')[3];
+			you114.style.left='0px';
+			var a4;
+			function you4(){  
+				var you1 = parseInt(you114.style.left);
+				if(you114.style.left=='-80px'){
+					clearInterval(a4);
+				}else{
+					you114.style.left=you1-1+'px';
+				}
+			}
+			function you41(){
+				var you11 = parseInt(you114.style.left);
+				if(you114.style.left=='0px'){
+					clearInterval(a4);
+				}else{
+					you114.style.left=you11+1+'px';
+				}
+			}
+			you114.onmouseover=function(){
+				clearInterval(a4);
+				a4=setInterval(you4,1);
+			}
+			you114.onmouseout=function(){
+				clearInterval(a4);
+				a4=setInterval(you41,1);
+			} 
+			var biao2 = document.getElementsByClassName('biao2')[0];
+			for(var i=1;i<14;i++){
+				var div = document.createElement('div');
+				var text = document.createTextNode("[公告] 帮购及VIP服务调整"+i);
+				div.appendChild(text);
+				div.setAttribute('class','biao3');
+				biao2.appendChild(div);
+			}
+			for(var i=1;i<12;i++){
+				var div = document.createElement('div');
+				var text = document.createTextNode("[公告] 帮购及VIP服务调整"+i);
+				div.appendChild(text);
+				div.setAttribute('class','biao3');
+				biao2.appendChild(div);
+			}
+			biao2.style.top='0px';
+			function gonggao(){
+				var top = parseInt(biao2.style.top);
+				if(biao2.style.top=='-355px'){
+					biao2.style.top='0px';
+				}else{
+					biao2.style.top=top-1+'px';
+				}
+			}
+			biao2.timer = setInterval(gonggao,25);
+			biao2.onmouseover=function(){
+				clearInterval(biao2.timer);
+			}
+			biao2.onmouseout=function(){
+				biao2.timer=setInterval(gonggao,25);
+			}
+	    //轮播图
+			var slider = document.getElementById('slider');
+			var left21 = document.getElementById('left21');
+			var left = document.getElementById('left');
+			var right = document.getElementById('right');
+			var oNavlist = document.getElementById('nav').children;
+			var index=1;
+			function next(){
+				index++;
+				animate(slider,{left:-809*index},function(){
+					if(index==7){
+						slider.style.left = '-809px';
+						index=1;
+					}
+				});
+				navChange();
+			}
+			function prev(){
+				index--;
+				animate(slider,{left:-809*index},function(){
+					if(index==0){
+						slider.style.left = '-4045px';
+						index=6;
+					}
+				});	
+				navChange();	
+			}
+			var timer = setInterval(next,2000);
+			left21.onmouseover = function(){
+				clearInterval(timer);
+				animate(left,{opacity:100});
+				animate(right,{opacity:100});
+
+			}
+			left21.onmouseout = function(){
+				animate(left,{opacity:0});
+				animate(right,{opacity:0});
+				timer = setInterval(next,3000);
+			}
+			right.onclick = next;
+			left.onclick = prev;
+			for(var i=0;i<oNavlist.length;i++){
+				oNavlist[i].idx = i;
+				oNavlist[i].onclick = function(){
+					index = this.idx+1;
+					animate(slider,{left:-809*index});
+					navChange();
+				}
+			}
+			function navChange(){
+				for(var i=0;i<oNavlist.length;i++){
+					oNavlist[i].className='';
+				}
+				if(index==7){
+					oNavlist[0].className='active';
+				}
+				else if(index==0){
+					oNavlist[5].className='active';
+				}else{
+					oNavlist[index-1].className='active';
+				}
+			}		
+			var coin1 = document.getElementById('coin1');
+			var coin2 = document.getElementById('coin2');
+			var button = document.getElementsByClassName('button')[0];
+			coin1.onchange=function(){
+				coin2.innerHTML="￥"+" "+coin1.value;
+			}
+			button.onclick=function(){
+				alert(coin1.value+"元话费充值成功！");
+			}
+			window.onload = function(){
+				var one_0 = document.getElementsByClassName('one_0')[0];
+				window.onscroll=function(){
+					var st = document.documentElement.scrollTop || document.body.scrollTop;
+					if(st>100){
+						one_0.style.position = 'fixed';						
+						one_0.style.backgroundColor="white";
+					}else{
+						one_0.style.position = 'static';
+					}
+				}
+			}
+			function animate(obj,json,callback){
+				clearInterval(obj.timer);
+				obj.timer = setInterval(
+					function(){	
+						var isStop=true;
+					    for(var attr in json){
+					    	if(attr=='opacity'){
+					    		var now = parseInt(100*getStyle(obj,attr));
+					    	}else{
+					    		var now = parseInt(getStyle(obj,attr));
+					    	}							
+						    var speed=(json[attr]-now)/5;
+						    speed = speed>0?Math.ceil(speed):Math.floor(speed);
+						    if(attr=='opacity'){
+						    	obj.style[attr] = (now+speed)/100;
+						    }else{
+						    	obj.style[attr] = now+speed+'px';
+						    }													    
+						    var current=now+speed;
+						    if(json[attr]!=current){
+						    	isStop=false;
+						    }
+						}
+						if(isStop){
+							clearInterval(obj.timer);
+							callback&&callback();
+						}													
+					},20);
+			}
+			function getStyle(obj,style){
+				if(obj.currentStyle){
+					return obj.currentStyle[style];
+				}
+				else{
+					return getComputedStyle(obj)[style];
+				}
+			}
---
+++ weber/js/index2.js
@@ -0,0 +1,143 @@
+			window.onload = function(){
+				var one_0 = document.getElementsByClassName('one_0')[0];
+				window.onscroll=function(){
+					var st = document.documentElement.scrollTop || document.body.scrollTop;
+					if(st>230){
+						one_0.style.position = 'fixed';			
+						one_0.style.backgroundColor="white";
+					}else{
+						one_0.style.position = 'static';
+					}
+				}
+			}
+			var s= document.getElementsByClassName('slid')[0];
+		 	var l = document.getElementsByClassName('left11')[0];	
+		 	var big= document.getElementsByClassName('big')[0];
+		 	var Bimg = document.getElementsByClassName('Bimg')[0];
+		 	var white = document.getElementsByClassName('white')[0];
+		 	l.onmouseover = function(){
+		 		s.style.display='block';
+		 		big.style.display='block';
+		 	}
+		 	l.onmouseout = function(){
+		 		s.style.display='none';
+		 		big.style.display='none';
+		 	}
+		 	l.onmousemove = function(ev){
+		 		var length = ev.clientY-237+getScrollTop()-s.offsetHeight/2;
+		 		var oL = ev.clientX-white.offsetLeft-15-s.offsetWidth/2;		
+		 		var oT = getScrollTop()>200?length+150:length;
+		 	    var oMaxw = l.offsetWidth-s.offsetWidth;
+		 		var oMaxh = l.offsetHeight-s.offsetHeight;
+		 		oL = oL>oMaxw?oMaxw:oL<0?0:oL;
+		 		oT = oT>oMaxh?oMaxh:oT<0?0:oT; 		
+		 		s.style.left=oL+'px';
+		 		s.style.top=oT+'px';
+		 		Bimg.style.left='-'+oL+'px';
+		 		Bimg.style.top='-'+oT+'px'; 		
+		 	}
+		 	function getScrollTop(){  
+	        	var scrollPos;  
+	        	if (window.pageYOffset) {  
+	        		scrollPos = window.pageYOffset;
+	        	}  
+	       		else if (document.compatMode && document.compatMode != 'BackCompat'){ 
+	        		scrollPos = document.documentElement.scrollTop;
+	        	}  
+	        	else if (document.body) { 
+	        		scrollPos = document.body.scrollTop;
+	        	}   
+	        	return scrollPos;   
+			}
+			var left291 = document.getElementsByClassName('left291')[1];
+			var a0 = document.getElementsByClassName('a0')[0];
+			var a1 = document.getElementsByClassName('a1')[0];
+			var a2 = document.getElementsByClassName('a11')[0];
+			var a3 = document.getElementsByClassName('a121')[0];
+			left291.onclick=function(){
+				a0.style.display="block";
+				a1.style.display="block";
+			}
+			a2.onclick=function(){
+				a0.style.display="none";
+				a1.style.display="none";
+			}
+			a3.onclick=function(){
+				a0.style.display="none";
+				a1.style.display="none";
+			}
+			var add = document.getElementById('add');
+			var left263 = document.getElementsByClassName('left263')[0];
+			var jian = document.getElementById('jian');			
+			add.onmouseover=function(){
+				if(left263.value<5){
+					add.style.cursor="pointer";
+				}else{
+					add.style.cursor="not-allowed";
+				}
+			}
+			add.onclick=function(){
+				if(left263.value<5){
+					left263.value++;
+				}	
+				else{
+					add.style.cursor="not-allowed";
+				}			
+			}
+			jian.onmouseover=function(){
+				if(left263.value>1){
+					jian.style.cursor="pointer";
+				}else{
+					jian.style.cursor="not-allowed";
+				}			
+			}
+			jian.onclick=function(){
+				if(left263.value>1){
+					left263.value--;
+				}else{
+					jian.style.cursor="not-allowed";
+				}					
+			}
+			var left252 = document.getElementsByClassName('left252')[0];
+			var left253 = document.getElementsByClassName('left253')[0];
+			var ml150 = document.getElementById('ml150');
+			left252.onclick=function(){
+				ml150.innerHTML=""+left252.innerHTML;
+				left253.style.background="no-repeat bottom right";
+				left253.style.borderColor="white";
+				left252.style.borderColor="#ff0853";
+			}
+			left253.onclick=function(){
+				ml150.innerHTML=left253.innerHTML;
+				left252.style.background="no-repeat bottom right";
+				left252.style.borderColor="white";
+				left253.style.borderColor="#ff0853";
+			}
+			var imgpp = document.getElementById('imgpp');
+			var bigpp = document.getElementById('bigpp');
+			var left120 = document.getElementById('left120');
+			var left123 = document.getElementById('left123');
+			var left121 = document.getElementsByClassName('left121')[0];
+			var left122 = document.getElementsByClassName('left122')[0];
+			var flag = 1;
+			function a(){
+				imgpp.src='../img/pp0.jpeg';
+				bigpp.src='../img/big1.jpg';
+				left121.style.borderColor="#ff9003";
+				left122.style.borderColor="white";	
+				flag = 1;											
+			}
+			function b(){
+				imgpp.src='../img/pp1.jpeg';
+				left121.style.borderColor="white";
+				left122.style.borderColor="#ff9003";
+				flag = 0;
+			}			
+			function c(){
+				if(flag==1){
+					b();
+				}else{
+					a();
+				}
+			}	
+			
